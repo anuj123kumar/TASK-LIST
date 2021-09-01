70f6f18ae8f5a8f1020a0d776f15d1d64ebbefd1
Anuj kumar <anujkumar41396502818@gmail.com>
2021-09-01 21:18:13 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const port = process.env.port || 8000;
+
+//app.use(express.json())
+app.use(cors())
+
+const databaseURL = 'mongodb+srv://Anuj:todo123@cluster0.z2vve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(databaseURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('DB CONNECTED')
+})
+.catch( (err) => {
+    console.log(err)
+})
+
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+}) 
